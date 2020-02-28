7606d43864e4614b005d3303c0bf0aaed46e0885
Hashim <42122270+DefyTheRush@users.noreply.github.com>
2020-02-28 08:13:07 -0500

    Add files for ProcessLogger v0.1

---
+++ ProcessLogger/ProcessLogger/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace ProcessLogger
+{
+    partial class ProcessLogger
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ProcessLogger));
+            this.MnuStrip = new System.Windows.Forms.MenuStrip();
+            this.mainToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.restartToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.LstProcessWindow = new System.Windows.Forms.ListBox();
+            this.TmrLoadProcesses = new System.Windows.Forms.Timer(this.components);
+            this.MnuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MnuStrip
+            // 
+            this.MnuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mainToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.MnuStrip.Location = new System.Drawing.Point(0, 0);
+            this.MnuStrip.Name = "MnuStrip";
+            this.MnuStrip.Size = new System.Drawing.Size(800, 24);
+            this.MnuStrip.TabIndex = 0;
+            this.MnuStrip.Text = "Menu";
+            // 
+            // mainToolStripMenuItem
+            // 
+            this.mainToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.restartToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.mainToolStripMenuItem.Name = "mainToolStripMenuItem";
+            this.mainToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.mainToolStripMenuItem.Text = "Main";
+            // 
+            // restartToolStripMenuItem
+            // 
+            this.restartToolStripMenuItem.Name = "restartToolStripMenuItem";
+            this.restartToolStripMenuItem.Size = new System.Drawing.Size(110, 22);
+            this.restartToolStripMenuItem.Text = "Restart";
+            this.restartToolStripMenuItem.Click += new System.EventHandler(this.restartToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(110, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // LstProcessWindow
+            // 
+            this.LstProcessWindow.Enabled = false;
+            this.LstProcessWindow.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LstProcessWindow.FormattingEnabled = true;
+            this.LstProcessWindow.HorizontalScrollbar = true;
+            this.LstProcessWindow.Location = new System.Drawing.Point(17, 42);
+            this.LstProcessWindow.Name = "LstProcessWindow";
+            this.LstProcessWindow.SelectionMode = System.Windows.Forms.SelectionMode.None;
+            this.LstProcessWindow.Size = new System.Drawing.Size(766, 342);
+            this.LstProcessWindow.TabIndex = 1;
+            // 
+            // TmrLoadProcesses
+            // 
+            this.TmrLoadProcesses.Interval = 1000;
+            this.TmrLoadProcesses.Tick += new System.EventHandler(this.TmrLoadProcesses_Tick);
+            // 
+            // ProcessLogger
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 399);
+            this.Controls.Add(this.LstProcessWindow);
+            this.Controls.Add(this.MnuStrip);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.MnuStrip;
+            this.MaximizeBox = false;
+            this.Name = "ProcessLogger";
+            this.Text = "Process Logger | By DefyTheRush";
+            this.Load += new System.EventHandler(this.ProcessLogger_Load);
+            this.MnuStrip.ResumeLayout(false);
+            this.MnuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip MnuStrip;
+        private System.Windows.Forms.ToolStripMenuItem mainToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem restartToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ListBox LstProcessWindow;
+        private System.Windows.Forms.Timer TmrLoadProcesses;
+    }
+}
+
---
+++ ProcessLogger/ProcessLogger/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections;
+using System.Diagnostics;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ProcessLogger
+{
+    public partial class ProcessLogger : Form
+    {
+        ArrayList processList = new ArrayList();
+        long counter = 0;
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        private static extern Int32 GetWindowThreadProcessId(IntPtr cur, out uint processId);
+
+        public ProcessLogger()
+        {
+            InitializeComponent();
+        }
+
+        private void ProcessLogger_Load(object sender, EventArgs e)
+        {
+            TmrLoadProcesses.Start();
+        }
+
+        private void restartToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Created by DefyTheRush", "About");
+        }
+
+        private void TmrLoadProcesses_Tick(object sender, EventArgs e)
+        {
+            switch (counter)
+            {
+                case 0:
+                    String process = GetProcessName();
+                    if (!(process.Equals("")))
+                    {
+                        LstProcessWindow.Items.Add(process);
+                        counter++;
+                    }
+                    process = null;
+                    //GC.Collect();
+                    break;
+                default:
+                    process = GetProcessName();
+                    if (!(process.Equals("")))
+                    {
+                        if (process.Equals(LstProcessWindow.Items[LstProcessWindow.Items.Count - 1]))
+                        {
+
+                        }
+                        else
+                        {
+                            LstProcessWindow.Items.Add(process);
+                            counter++;
+                        }
+                        process = null;
+                        //GC.Collect();
+                    }
+                    break;
+            }
+        }
+
+        private string GetProcessName()
+        {
+            IntPtr cur = GetForegroundWindow();
+
+            if (cur == null)
+            {
+                return "";
+            }
+            else
+            {
+                uint pId;
+                GetWindowThreadProcessId(cur, out pId);
+
+                foreach (System.Diagnostics.Process pro in System.Diagnostics.Process.GetProcesses())
+                {
+                    if (pro.Id == pId)
+                    {
+                        if (pro.ProcessName.Equals("ShellExperienceHost"))
+                        {
+                            return "";
+                        }
+                        else if (pro.ProcessName.Equals("ApplicationFrameHost"))
+                        {
+                            return pro.MainWindowTitle;
+                        }
+                        else
+                        {
+
+                            return pro.ProcessName;
+                        }
+                    }
+                    //GC.Collect();
+                }
+            }
+            return "";
+        }
+
+        private void LstProcessWindow_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
